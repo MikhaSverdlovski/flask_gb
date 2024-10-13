59798dd0066f43d5f18c2e66b15659d30613d82f
MikhaSverdlovski <shirinkin.mikhail1992@gmail.com>
2024-10-13 21:09:02 +0300

    register

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 import datetime
 
 from flask import Flask
-
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired, Length, NumberRange
 
 app = Flask(__name__)
+
+
+class registerForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=3, max=20)])
+    age = IntegerField(validators=[InputRequired(), NumberRange(min=0, max=100)])
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    form = registerForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        age = form.age.data
+        return f'<h1>Hello {username} {age}!</h1>'
+    else:
+        return f'<h1>Invalid username or age</h1><br> {form.errors}', 400
+
+@app.route('/uptime', methods=['GET'])
+def uptime():
+    uptime = datetime.datetime.now()
+    return str(uptime)
 
 
 @app.route('/')
@@ -26,6 +48,5 @@ def time_func():
 
 
 if __name__ == '__main__':
+    app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True, port=8000)
-
-
